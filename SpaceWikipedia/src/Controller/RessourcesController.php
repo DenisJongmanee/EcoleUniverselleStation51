51e56c1e31c7ab82dd162ec7186212517c1440f5
DenisJongmanee <jongmanee.denis@gmail.com>
2021-12-12 23:12:44 +0100

    base list ressources controller

--- SpaceWikipedia/src/Controller/RessourcesController.php
+++ SpaceWikipedia/src/Controller/RessourcesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class RessourcesController extends AbstractController
 {
     #[Route('/ressources', name: 'ressources')]
-    public function index(): Response
+    public function list(ManagerRegistry $doctrine): Response
     {
+
+        $manager = $doctrine->getManager();
+
+        $repositoryArticle = $manager->getRepository(Article::class);
+        
+        $articles = $repositoryArticle->findAll();
+        
+        dump($articles);
+
         return $this->render('ressources/index.html.twig', [
-            'controller_name' => 'RessourcesController',
+            'articles' => $articles
         ]);
     }
 }
