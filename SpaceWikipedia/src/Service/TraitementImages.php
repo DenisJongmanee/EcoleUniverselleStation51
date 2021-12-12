7677a53975f16ef853fdd4d1227fbc111612d305
Vonkuro <vonkuro@gmail.com>
2021-12-12 13:15:20 +0100

    enregistrement des images, ranger par id article

--- SpaceWikipedia/src/Service/TraitementImages.php
+++ SpaceWikipedia/src/Service/TraitementImages.php
@@ -1,12 +1,29 @@
 <?php
 namespace App\Service;
 
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
 class TraitementImages
 {
-    public function telechargeImage(string $titre, string $url)
+    private $fileManager;
+
+
+    public function __construct(Filesystem $fileManager)
     {
+        $this->fileManager = $fileManager;
+    }
+
+    public function telechargeImage(int $articleId, string $titre, string $url)
+    {
+        $pathDossier = "..\images\Article N°" . strval($articleId);
+        if (! $this->fileManager->exists($pathDossier))
+        {
+            $this->fileManager->mkdir($pathDossier);
+        }
         $donneeImage = file_get_contents($url);
-        $path = "..\images\\" . $titre;
-        file_put_contents($path,$donneeImage);
+        $pathImage = $pathDossier . "\\" . $titre;
+        file_put_contents($pathImage,$donneeImage);
     }
 }
