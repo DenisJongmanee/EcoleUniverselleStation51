bf487cd4e1d7dbc86c30dbccdd8c43ad5d883848
DenisJongmanee <jongmanee.denis@gmail.com>
2021-12-13 20:05:42 +0100

    add template item ressource (page 3) and update list

--- SpaceWikipedia/src/Controller/RessourcesController.php
+++ SpaceWikipedia/src/Controller/RessourcesController.php
@@ -12,25 +12,40 @@ use App\Repository\ArticleRepository;
 
 class RessourcesController extends AbstractController
 {
-    #[Route('/ressources', name: 'ressources')]
+    #[Route('/ressources', name: 'listRessources')]
     public function list(ManagerRegistry $doctrine, Request $request): Response
     {
+        #redirection vers l'accueil si le critère n'est pas défini
         if (!$request->query->get('search')) 
         {
             return $this->redirectToRoute('accueil');
         }
 
+        #on récupère le critère du champ
         $title = $request->query->get('search');
         
         $repositoryArticle = new ArticleRepository($doctrine);
         
         $articles = $repositoryArticle->findAllByTitle($title);
-        
+
         dump($articles);
 
         return $this->render('ressources/index.html.twig', [
             'articles' => $articles,
             'search' => $title
+        ]);
+    }
+
+    #[Route('/ressources/{id}', name:"itemRessource")]
+    public function item(ManagerRegistry $doctrine, int $id) : Response
+    {
+       
+        $entityManager = $doctrine->getManager();
+        $repositoryArticle = $entityManager->getRepository(Article::class);
+        $article = $repositoryArticle->find($id);
+        dump($article);
+        return $this->render('ressources/item.html.twig', [
+            "article" => $article
         ]);
     }
 }
