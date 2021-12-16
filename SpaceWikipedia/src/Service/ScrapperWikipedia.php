1bf159360ecbed79d85e54db4070acf73e057701
Vonkuro <vonkuro@gmail.com>
2021-12-16 21:05:10 +0100

    Scrapper v1

--- SpaceWikipedia/src/Service/ScrapperWikipedia.php
+++ SpaceWikipedia/src/Service/ScrapperWikipedia.php
@@ -3,5 +3,26 @@ namespace App\Service;
 
 class ScrapperWikipedia
 {
-    
+
+    private $htmlManager;
+    private $imageManager;
+    private $lienManager;
+
+    public function __construct(GetHTML $htmlManager, TraitementImages $imageManager, TraitementLiens $lienManager)
+    {
+        $this->htmlManager = $htmlManager;
+        $this->imageManager = $imageManager;
+        $this->lienManager = $lienManager;
+    }
+
+    public function scrapListe(array $listeUrl)
+    {
+        foreach ($listeUrl as $url)
+        {
+            $html = $this->htmlManager->getBody($url);
+            $article = $this->htmlManager->enregistrerArticle($html);
+            $article = $this->imageManager->remplacementImagesArticle($article);
+            $article = $this->lienManager->remplacementLiens($article);
+        }
+    }
 }
