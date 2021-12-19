9fcc13a10715e189079361ee5b1fa725b659dc30
Vonkuro <vonkuro@gmail.com>
2021-12-19 14:40:02 +0100

    création de l'admin

--- SpaceWikipedia/src/DataFixtures/AppFixtures.php
+++ SpaceWikipedia/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder=$encoder;
+
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        $admin = new User();
+        $admin->setEmail('admin@gmail.com');
+        $admin->setPassword($this->encoder->hashPassword($admin, "adminadmin"));
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
